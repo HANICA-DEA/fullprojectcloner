d7ee7cfb3e090b81a35d9bdfc360dd640fb1560d
Rick Rietveld <R.Rietveld@student.han.nl>
2018-11-16 14:26:39 +0100

    Created standard working template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CloneComponent } from './clone/clone.component';
-import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {RepositoriesComponent} from './repositories/repositories.component';
+import {ContactComponent} from './contact/contact.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 
@@ -20,7 +20,7 @@ import {MatInputModule} from '@angular/material/input';
     HeaderComponent,
     HomeComponent,
     AboutComponent,
-    CloneComponent,
+    RepositoriesComponent,
     ContactComponent
   ],
   imports: [
