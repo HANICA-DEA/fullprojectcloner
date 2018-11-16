4e55de691d29da34aae25106ad6897a8e4e9c733
Rick Rietveld <R.Rietveld@student.han.nl>
2018-11-16 15:35:31 +0100

    Created base template (once again)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CloneComponent } from './clone/clone.component';
-import { ContactComponent } from './contact/contact.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {RepositoriesComponent} from './repositories/repositories.component';
+import {ContactComponent} from './contact/contact.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'clone', component: CloneComponent},
+  {path: 'repositories', component: RepositoriesComponent},
   {path: 'contact', component: ContactComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CloneComponent } from './clone/clone.component';
-import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {RepositoriesComponent} from './repositories/repositories.component';
+import {ContactComponent} from './contact/contact.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import {MatExpansionModule} from '@angular/material/expansion';
@@ -21,7 +21,7 @@ import {MatExpansionModule} from '@angular/material/expansion';
     HeaderComponent,
     HomeComponent,
     AboutComponent,
-    CloneComponent,
+    RepositoriesComponent,
     ContactComponent
   ],
   imports: [
