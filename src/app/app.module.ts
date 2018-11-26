8649fc407d8b97e3aaa531bcb25131cad176ae72
Rick Rietveld <R.Rietveld@student.han.nl>
2018-11-26 11:55:27 +0100

    MAIL WORKS!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {environment} from '../environments/environment';
 import {LoginComponent} from './login/login.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from "@angular/http";
 
 
 @NgModule({
@@ -47,7 +47,7 @@ import {HttpClientModule} from '@angular/common/http';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
-    HttpClientModule,
+    HttpModule,
     ReactiveFormsModule,
     FormsModule
   ],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import {ContactFormDto} from '../services/dto/contactform.dto';
-
-// const headers = new Headers({
-//   'Accept': 'application/json',
-//   'enctype': 'multipart/form-data'
-// })
-//   // .set('Content-Type', 'undefined')
-//   // .set('Access-Control-Allow-Origin', '*')
-//   // .set('Access-Control-Allow-Methods', 'POST')
-//   // .set('Access-Control-Allow-Headers', 'Content-type')
-//   // .set('Access-Control-Allow-Credentials', 'true');
-
+import {Headers, Http} from '@angular/http';
 
 @Component({
   selector: 'app-contact',
@@ -20,20 +8,16 @@ import {ContactFormDto} from '../services/dto/contactform.dto';
   styleUrls: ['./contact.component.sass']
 })
 
-
 export class ContactComponent implements OnInit {
-
-  headers = new Headers({'Content-Type': 'application/json;charset=utf-8'});
 
   contactForm: FormGroup;
   submitted = false;
 
-
   MAIL_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxrBysjuMbBGdWRyaOXpW2PwkavvyLePxSmQeJC5CdAQEHS3ys/exec';
 
-  private contactFormDto: ContactFormDto;
+  private body: string;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+  constructor(private formBuilder: FormBuilder, private http: Http) {
     this.contactForm = this.formBuilder.group({
       fullName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -41,51 +25,20 @@ export class ContactComponent implements OnInit {
     });
   }
 
-
   onSubmit() {
-
-
-    // let body = JSON.stringify('hello');
-    //
-    // let url = 'https://script.google.com/macros/s/AKfycbxrBysjuMbBGdWRyaOXpW2PwkavvyLePxSmQeJC5CdAQEHS3ys/exec';
-    //
-    // this.http.post(url, body).subscribe(data => {
-    //       console.log('Mail has been sent' + data);
-    //     }, error => {
-    //       console.log('Mail has not been sent ' + error);
-    //     });
 
 
     this.submitted = true;
 
-    if (this.contactForm.invalid) {
-      return;
-    }
+    this.body = '&name=' + this.contactForm.controls.fullName.value + '&email=' +
+      this.contactForm.controls.email.value + '&message=' + this.contactForm.controls.message.value;
 
-    if (this.contactForm.valid) {
-      this.contactFormDto = new ContactFormDto(
-        this.contactForm.controls.fullName.value,
-        this.contactForm.controls.email.value,
-        this.contactForm.controls.message.value
-      );
-
-
-      this.http.post(
-        this.MAIL_SCRIPT_URL,
-        JSON.stringify("hello")).subscribe(data => {
-        console.log('Mail has been sent' + data);
-      }, error => {
-        console.log('Mail has not been sent ' + error);
+    const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+    this.http.post(this.MAIL_SCRIPT_URL, this.body, {headers: headers})
+      .subscribe((response) => {
+        console.log(response);
       });
     }
-  }
-
-
-  //
-  // console.log(
-  //   'fullName: ' + this.contactForm.controls.fullName.value +
-  //   ' email: ' + this.contactForm.controls.email.value +
-  //   ' message: ' + this.contactForm.controls.message.value);
 
   ngOnInit() {
   }
