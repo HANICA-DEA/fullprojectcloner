be9ccfa4b0a31adeec131e2f8aba054e30c067fb
Joost Kaal <joost.kaal@hotmail.nl>
2018-11-30 10:37:46 +0100

    Added invite mail function (no script attatched)

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from '../../services/data/data.service';
 import {AuthService} from '../../services/auth/auth.service';
+import {Headers, Http} from '@angular/http';
+import {InviteFormDto} from '../../services/dto/inviteform.dto';
 
 @Component({
   selector: 'app-repositories',
@@ -13,8 +15,10 @@ export class RepositoriesComponent implements OnInit {
   repositories: any;
   public searchedUserExists: boolean;
   public data: DataService;
+  INVITEMAIL_SCRIPT_URL = 'https://script.google.com/macros/s/{{{{{vulhieronsidinvanhetscript}}}}}/exec';
+  private inviteFormDto: InviteFormDto;
 
-  constructor(data: DataService, public authService: AuthService) {
+  constructor(data: DataService, public authService: AuthService, private http: Http) {
     this.data = data;
   }
 
@@ -30,6 +34,16 @@ export class RepositoriesComponent implements OnInit {
     }, err => {
       this.searchedUserExists = false;
     });
+  }
+
+
+  sendInviteToUser(emailaddress: string) {
+    const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+    this.inviteFormDto = new InviteFormDto(emailaddress);
+    this.http.post(this.INVITEMAIL_SCRIPT_URL, this.inviteFormDto, {headers: headers})
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
 }
