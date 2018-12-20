6d0e658efa5cd2549d75bac7f3733c053418394f
TimMaasGeesteranus <38353198+TimMaasGeesteranus@users.noreply.github.com>
2018-12-20 16:01:00 +0100

    test in repository-button aan de praat gekregen

--- src/app/components/repositories/repository-button/repository-button.component.spec.ts
+++ src/app/components/repositories/repository-button/repository-button.component.spec.ts
@@ -1,6 +1,32 @@
 import {RepositoryButtonComponent} from './repository-button.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RepositorySendinviteComponent} from '../repository-sendinvite/repository-sendinvite.component';
+import {MatCardModule, MatFormFieldModule} from '@angular/material';
 
 describe('RepositoryButtonComponent', () => {
-  it('empty test', () => {
+  let component: RepositoryButtonComponent;
+  let fixture: ComponentFixture<RepositoryButtonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule
+      ],
+      declarations: [
+        RepositoryButtonComponent,
+        RepositorySendinviteComponent
+      ]
+    });
+
+    fixture = TestBed.createComponent(RepositoryButtonComponent);
+    component = fixture.componentInstance;
+  }));
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
   });
 });
