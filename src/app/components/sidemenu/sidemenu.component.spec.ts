f098bde0d4e8422174ef86b0cc1598e862629fef
TimMaasGeesteranus <38353198+TimMaasGeesteranus@users.noreply.github.com>
2019-01-07 10:25:19 +0100

    Sidemenu tested

--- src/app/components/sidemenu/sidemenu.component.spec.ts
+++ src/app/components/sidemenu/sidemenu.component.spec.ts
@@ -1,6 +1,36 @@
 import {SidemenuComponent} from './sidemenu.component';
+import {RepositoryButtonComponent} from '../repositories/repository-button/repository-button.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule, MatFormFieldModule} from '@angular/material';
+import {RepositorySendinviteComponent} from '../repositories/repository-sendinvite/repository-sendinvite.component';
 
 describe('SidemenuComponent', () => {
+  let component: SidemenuComponent;
+  let fixture: ComponentFixture<SidemenuComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+
+      ],
+      declarations: [
+        SidemenuComponent,
+      ]
+    });
+
+    fixture = TestBed.createComponent(SidemenuComponent);
+    component = fixture.componentInstance;
+  }));
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+
   it('empty test', () => {
+    component.ngOnInit();
+    const currentYear = new Date().getFullYear();
+    expect(component.currentYear).toEqual(currentYear);
   });
 });
