7865c8630f8809fcbd8c6a5043eec4f374c99cc8
TimMaasGeesteranus <38353198+TimMaasGeesteranus@users.noreply.github.com>
2019-01-07 13:43:07 +0100

    repository-picker fix

--- src/app/components/repositories/repository-picker/repository-picker.component.spec.ts
+++ src/app/components/repositories/repository-picker/repository-picker.component.spec.ts
@@ -1,63 +1,46 @@
-// import {RepositoryPickerComponent} from './repository-picker.component';
-// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-// import {GithubService} from '../../../services/github/github.service';
-//
-//
-// describe('RepositoryPickerComponent', () => {
-//   let component = RepositoryPickerComponent;
-//   let fixture: ComponentFixture<RepositoryPickerComponent>;
-//
-//   const githubService: GithubService;
-//   // const templates: Template[] = [];
-//
-//
-//   // beforeEach(async(() => {
-//   //   TestBed.configureTestingModule({
-//   //     declarations: [RepositoryPickerComponent]
-//   //   });
-//   //
-//   //   fixture = TestBed.createComponent(RepositoryPickerComponent);
-//   //   component = [fixture.componentInstance],
-//   //     providers: [
-//   //     LoggingService,
-//   //     SheetService,
-//   //     TemplateRepository
-//   //   ]
-//   // }));
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         fixture.componentInstance
-//       ],
-//       providers: [
-//         GithubService
-//       ]
-//     })
-//       .compileComponents();
-//   }));
-//
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RepositoryPickerComponent);
-//     component = fixture.componentInstance;
-//
-//     sheetService = TestBed.get(SheetService);
-//
-//     this.templates = [new Template(), new Template()];
-//
-//     spyOn(sheetService, 'getTemplates')
-//       .and.returnValue(Promise.resolve(templates));
-//   });
-//
-//   it('should be created', () => {
-//     expect(component).toBeTruthy();
-//   });
-//
-//   it('should have the templates set after component is initialized', fakeAsync(() => {
-//     fixture.detectChanges();
-//     tick();
-//     fixture.detectChanges();
-//     expect(component.templates).toBe(templates);
-//   }));
-// });
+import {async, ComponentFixture, fakeAsync, flushMicrotasks, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule, MatFormFieldModule} from '@angular/material';
+import {RepositoryPickerComponent} from './repository-picker.component';
+import {RepositoryInviteComponent} from '../repository-invite/repository-invite.component';
+
+describe('RepositoryPickerComponent', () => {
+  let component: RepositoryPickerComponent;
+  let fixture: ComponentFixture<RepositoryPickerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatFormFieldModule
+      ],
+      declarations: [
+        RepositoryPickerComponent,
+        RepositoryInviteComponent
+      ]
+    });
+
+    fixture = TestBed.createComponent(RepositoryPickerComponent);
+    component = fixture.componentInstance;
+  }));
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  // xit('repositorybutton calls chooserepository', async(() => {
+  //   // Hoe vul ik in godsnaam repositories met iets waardoor de test slaagt ):
+  //   // object: Object = "hoi";
+  //   component.showAllRepositories = true;
+  //   // component.repositories = object;
+  //   spyOn(component, 'chooseRepository');
+  //   fixture.detectChanges();
+  //   const button = fixture.debugElement.nativeElement.querySelector('#repository-button');
+  //   button.click();
+  //   expect(component.chooseRepository).toHaveBeenCalled();
+  // }));
+
+});
