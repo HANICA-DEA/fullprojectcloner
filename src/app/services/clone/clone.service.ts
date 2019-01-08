0da3da1e9d8352b318b267ea7b13b012b4ba7673
Jim van de Ligt <J.vandeLigt@student.han.nl>
2019-01-08 11:56:43 +0100

    Small refactors

--- src/app/services/clone/clone.service.ts
+++ src/app/services/clone/clone.service.ts
@@ -8,7 +8,6 @@ import {AuthdataDto} from '../../entities/auth/authdata.dto';
   providedIn: 'root'
 })
 export class CloneService {
-
   constructor(private readonly github: GithubService) {
   }
 
@@ -17,18 +16,21 @@ export class CloneService {
     const token = auth.token;
     const recipientName = auth.username;
     const senderName = request._username;
-    const sourceReposioryName = request._repositoryName;
-    const newRepositoryName = sourceReposioryName + '-' + recipientName;
+    const sourceRepositoryName = request._repositoryName;
+    const newRepositoryName = sourceRepositoryName + '-' + recipientName;
 
+    // Create repository
     await this.github.persistRepository(token, newRepositoryName)
       .then(async Reporesponse => {
         if (Reporesponse != null) {
+          // Import repository data in new repository
           const impStarted = await this.github.importRepository
-          (token, recipientName, sourceReposioryName, new ImportDto(request._URL))
-            .toPromise();
+          (token, recipientName, sourceRepositoryName, new ImportDto(request._URL)).toPromise();
         }
       });
-    const projects = await this.github.getProjects(token, senderName, sourceReposioryName).toPromise();
+
+    // Obtain projects
+    const projects = await this.github.getProjects(token, senderName, sourceRepositoryName).toPromise();
 
     let projectId: number;
     let columnId: number;
@@ -36,31 +38,40 @@ export class CloneService {
 
     for (let projectCount = 0; projectCount < Object.keys(projects).length; projectCount++) {
       const projectContent = {name: projects[projectCount].name, body: projects[projectCount].body};
+      // Create project
       await this.github.persistProject(token, recipientName, newRepositoryName, projectContent).then(async response => {
         console.log(response);
         projectId = JSON.parse(JSON.stringify(response)).id;
       });
+
+      // Obtain all columns.
       const columns = await this.github.getUsingUrl(token, projects[projectCount].columns_url).toPromise();
       for (let columnCount = 0; columnCount < Object.keys(columns).length; columnCount++) {
         const columnContent = {name: columns[columnCount].name};
         await this.github.persistColumn(token, projectId, columnContent).then(async response => {
           columnId = JSON.parse(JSON.stringify(response)).id;
         });
+
+        // Obtain all cards.
         const cards = await this.github.getUsingUrl(token, columns[columnCount].cards_url).toPromise();
         for (let cardCount = (Object.keys(cards).length - 1); cardCount > -1; cardCount--) {
-          if (cards[cardCount].note === null) {
+          if (cards[cardCount].note === null) { // If the card has no note, it's an issue
             const issue = await this.github.getUsingUrl(token, cards[cardCount].content_url).toPromise();
             const issueTitle = JSON.parse(JSON.stringify(issue)).title;
             const issueBody = JSON.parse(JSON.stringify(issue)).body;
             const issueContent = {title: issueTitle, body: issueBody};
 
+            // Create issue.
             await this.github.persistIssue(token, recipientName, newRepositoryName, issueContent).then(async response => {
               contentId = JSON.parse(JSON.stringify(response)).id;
             });
+
+            // Create card and link with issue.
             const cardsContent = {note: null, content_id: contentId, content_type: 'Issue'};
             await this.github.persistCard(token, columnId, cardsContent).then(async response => {
             });
           } else {
+            // Create card.
             const cardsContent = {note: cards[cardCount].note};
             await this.github.persistCard(token, columnId, cardsContent).then(async response => {
             });
